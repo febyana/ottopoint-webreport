86bb963c7707b7f5489e5ce4c04f80260f62eaf4
ahmad abidin <ahmad.abidin@ottodigital.id>
2019-12-13 00:26:01 +0700

    test webSocket

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { webSocket } from 'rxjs/webSocket';
 
 @Component({
   selector: 'app-admin',
@@ -8,11 +9,16 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements OnInit {
   mode = 'side';
+  subject = webSocket('ws://echo.websocket.org/');
+
   constructor(
     private router: Router,
   ) { }
 
   ngOnInit() {
+    this.subject.subscribe();
+    this.subject.next('ada kaga ya');
+    this.subject.complete();
     if (window.localStorage.getItem('token') == null) {
       this.router.navigateByUrl('/login');
     }
