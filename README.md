3ece016d8f17fc404e8421cb87ce23af7f021a06
ahmad abidin <ahmad.abidin@ottodigital.id>
2019-12-17 13:20:52 +0700

    change project name from "ottopoint-dashboard" to "v1.0"

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-test
+// lakukan SEBELUM ./deploy.sh dieksekusi
+cd /home/abidin
+rm v1.0.zip
+
+// lakukan SETELAH ./deploy.sh dieksekusi
+cd /opt/tomcat/webapps/ottopointweb
+rm v1.0.zip
+rm -rf v1.0
+
+cp /home/abidin/v1.0.zip .
+unzip v1.0.zip
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ottopoint-dashboard'),
+      dir: require('path').join(__dirname, './coverage/v1.0'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
