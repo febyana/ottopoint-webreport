ef209090582502cb3c6002df4bb8a047d2e144c2
dennisfinaldhi <dennis.finaldhi@gmail.com>
2020-03-16 16:20:15 +0700

    fixing redeem voucher opl

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -219,7 +219,7 @@ export interface TransactionVoucherRedeemOpl {
     customerLastname: string;
     customerPhone: string;
     customerEmail: string;
-    partner: string;
+    institution: string;
     vendor: string;
     costInPoints: number;
     campaignName: string;
